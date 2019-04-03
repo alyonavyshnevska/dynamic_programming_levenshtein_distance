6bc36b281505b05de9921aecc5da8cb824f62c6d
Alyona Vyshnevska <alyona.vyshnevska@gmail.com>
2019-04-03 09:56:33 +0200

    add wrking version of levanstein disctance code

---
+++ compute_levenshtein_distance.py
@@ -0,0 +1,32 @@
+def compute_levenshtein_distance(seq1, seq2):
+    '''
+    Calculates minimum edit distance between str1 and str2.
+    Uses dynamic programming. Stores interemediate results in a table.
+
+    :param str seq1: from this string
+    :param str seq2: into this string
+
+    :returns: edit distance
+    :rtype: int
+    '''
+
+    # Create a table to store intermediate results
+    table = [[0 for x in range(len(seq1)+1)] for x in range(len(seq2)+1)]
+
+    for row in range(len(seq2)+1):
+        for column in range(len(seq1)+1):
+
+            if row == 0:
+                table[row][column] = column
+
+            elif column == 0:
+                table[row][column] = row
+
+            elif seq2[row-1] == seq1[column-1]  :
+                table[row][column] = table[row-1][column-1]
+
+            else:
+                table[row][column] = 1 + min(table[row-1][column-1],
+                                            table[row][column-1],
+                                            table[row-1][column])
+    return table[row][column]
