688d6b62b8d21ba8597bbed091228268f8cbae00
Alyona Vyshnevska <alyona.vyshnevska@gmail.com>
2019-04-04 15:11:23 +0200

    add sanity check and trivial case tests

--- test.py
+++ test.py
@@ -3,18 +3,41 @@ from compute_levenshtein_distance import compute_levenshtein_distance
 
 compute_distance = compute_levenshtein_distance.compute_levenshtein_distance
 
+'''
+assert compute_levenshtein_distance("", "abc") == 3
+assert compute_levenshtein_distance("abc", "") == 3
+assert compute_levenshtein_distance("a", "a") == 0
+assert compute_levenshtein_distance("a", "b") == 1
+assert compute_levenshtein_distance("333", "3") == 2
+assert compute_levenshtein_distance("hello", "hili") == 3
+assert compute_levenshtein_distance("", "") == 0
+assert compute_levenshtein_distance("abc", "abc") == 0
+'''
+
 
 class TestSum(unittest.TestCase):
-    def test_shortest_distance(self):
+
+    def test_sanity_check(self):
         """
         Test that compute_levenshtein_distance produces the minimum number of edits
         to convert str1 into str2
         """
 
-        str1 = ""
-        str2 = "abc"
+        str1 = "ab"
+        str2 = "bc"
         shortest_distance = compute_distance(str1, str2)
-        self.assertEqual(shortest_distance, 3)
+        self.assertEqual(shortest_distance, 2)
+
+    def test_trivial_case(self):
+        """
+        Test that compute_levenshtein_distance produces the minimum number of edits
+        to convert str1 into str2
+        """
+
+        str1 = "a"
+        str2 = "a"
+        shortest_distance = compute_distance(str1, str2)
+        self.assertEqual(shortest_distance, 0)
 
 
 if __name__ == "__main__":
